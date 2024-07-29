7df5bd2ab4acd317e978852e78457d1419bd7c57
Rodrigo Alves <101226356+Driggsz@users.noreply.github.com>
2024-07-29 16:01:55 -0300

    Update Distribuição dos Scores.py

--- pages/Distribuição dos Scores.py
+++ pages/Distribuição dos Scores.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 # Carregar dados
 data = pd.read_csv('final_lead_scores.csv')  # Substitua pelo nome do seu arquivo
 
-st.header("Análise de Qualidade dos Leads por Criativo")
+
 
 # Média de Score por Criativo
 average_score = data.groupby('data.ad_name')['lead_score'].mean().reset_index()
@@ -14,24 +14,6 @@ average_score.columns = ['Criativo', 'Média do Score']
 # Exibir apenas os 10 principais criativos com maior média de score
 top_creatives = average_score.nlargest(10, 'Média do Score')
 
-# Análise adicional: Distribuição dos Scores por Criativo
-st.subheader("Distribuição dos Scores por Criativo")
-selected_creative = st.selectbox("Selecione um Criativo", data['data.ad_name'].unique())
-
-filtered_data = data[data['data.ad_name'] == selected_creative]
-
-st.write(f"Distribuição dos Scores para o Criativo: {selected_creative}")
-st.write(filtered_data['lead_score'].describe())
-
-# Histograma dos Scores para o Criativo Selecionado com Plotly
-fig_hist_creative = go.Figure(data=[go.Histogram(x=filtered_data['lead_score'], nbinsx=20, marker_color='lightgreen')])
-fig_hist_creative.update_layout(
-    title=f'Distribuição dos Scores para {selected_creative}',
-    xaxis_title='Score',
-    yaxis_title='Frequência',
-    bargap=0.2
-)
-st.plotly_chart(fig_hist_creative)
 
 # Distribuição Geral dos Scores
 st.header("Distribuição Geral dos Scores")
